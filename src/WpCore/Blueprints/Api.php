0b6f56578793f0957a8e4885095287e1bcb0a83c
Wassim <x.liquid1@gmail.com>
2020-01-23 17:56:30 +0100

    WP REST API controller added

---
+++ src/WpCore/Blueprints/Api.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ayctor\Controllers;
+
+use WpCore\Controllers\ApiController;
+
+class Blueprint extends ApiController
+{
+    public function register_routes()
+    {
+        //
+    }
+}
